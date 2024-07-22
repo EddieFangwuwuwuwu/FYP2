311a0f018b08cfea254114a36a20b179d1c5dcf6
EddieFangwuwuwuwu <hxfang956@gmail.com>
2024-07-22 14:54:00 +0800

    Sharing page modal added

--- components/Sharing.js
+++ components/Sharing.js
@@ -1,14 +1,28 @@
-import React from "react";
-import {Text,View,TouchableOpacity,StyleSheet} from 'react-native';
+import React,{useState} from "react";
+import {Text,View,TouchableOpacity,StyleSheet,Modal} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome'
 
 function SharingScreen(){
+  const [modalOpen, setModalOpen] = useState(false);
     return(
             <View style={styles.container}>
             <Text style={styles.emptyText}>No user to shared account yet</Text>
         <TouchableOpacity style={styles.addcategory} onPress={() => setModalOpen(true)}>
         <Icon name="plus" size={45} color="white" />
         </TouchableOpacity>
+        <Modal
+        visible={modalOpen}
+        animationType="slide"
+        transparent={true}
+      >
+        <View style={styles.modalOverlay}>
+          <View style={styles.modalContent}>
+            <TouchableOpacity style={styles.closeButton} onPress={() => setModalOpen(false)}>
+              <Icon name="close" size={45} color="black" />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
         </View>
     )
 
@@ -42,6 +56,25 @@ const styles= StyleSheet.create({
         height: 60,
         justifyContent: 'center',
         alignItems: 'center'
+      },
+
+      modalContent: {
+        width: '100%',
+        backgroundColor: 'white',
+        borderRadius: 10,
+        padding: 20,
+        alignItems: 'center'
+      },
+      modalOverlay: {
+        flex: 1,
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      closeButton: {
+        alignSelf: 'flex-end',
+        marginRight: 10,
+        marginTop: 10,
       },
 
 })
